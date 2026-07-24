263551142d0743a8c5c0dcddbbac36ff41641e7b
Chrisophy <darknobe@freenet.de>
2026-07-24 23:31:19 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,8 @@
 package com.template.htmlwrapper;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,17 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Statusleiste ausblenden (Vollbildmodus)
-        WindowInsetsControllerCompat controller = 
-                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
-        if (controller != null) {
-            // Blendet die Statusleiste oben aus
-            controller.hide(WindowInsetsCompat.Type.statusBars());
-            
-            // Optional: Zeigt die Leiste kurz an, wenn man vom oberen Rand nach unten wischt
-            controller.setSystemBarsBehavior(
-                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            );
+        // 1. Inhalt erlauben, bis unter die Systemleisten zu rendern
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        // 2. Kamera-Notch/Ausschnitt oben aktiv für Content freigeben (Android 9+)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
         }
 
         WebView webView = new WebView(this);
@@ -34,9 +32,21 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDomStorageEnabled(true);
         
         webView.setWebViewClient(new WebViewClient());
-        
         webView.loadUrl("https://chrisophy.github.io/GEZ-KiNO");
         
         setContentView(webView);
+
+        // 3. Statusleiste (Uhrzeit, Benachrichtigungen) ausblenden
+        WindowInsetsControllerCompat controller = 
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        if (controller != null) {
+            controller.hide(WindowInsetsCompat.Type.statusBars());
+            // Tipp: Wenn auch die untere Gesten-/Navigationsleiste weg soll:
+            // controller.hide(WindowInsetsCompat.Type.systemBars());
+            
+            controller.setSystemBarsBehavior(
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            );
+        }
     }
 }
