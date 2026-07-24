af3de5c4c4a412b896582f47e742fa8397d929e7
Chrisophy <darknobe@freenet.de>
2026-07-24 23:09:07 +0200

    Implement fullscreen mode in MainActivity
    Added fullscreen mode by hiding the status bar.

--- MainActivity.java
+++ MainActivity.java
@@ -5,11 +5,28 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Statusleiste ausblenden (Vollbildmodus)
+        WindowInsetsControllerCompat controller = 
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        if (controller != null) {
+            // Blendet die Statusleiste oben aus
+            controller.hide(WindowInsetsCompat.Type.statusBars());
+            
+            // Optional: Zeigt die Leiste kurz an, wenn man vom oberen Rand nach unten wischt
+            controller.setSystemBarsBehavior(
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            );
+        }
+
         WebView webView = new WebView(this);
         WebSettings webSettings = webView.getSettings();
         
@@ -18,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         
         webView.setWebViewClient(new WebViewClient());
         
-        // HIER die gewünschte URL eintragen:
         webView.loadUrl("https://chrisophy.github.io/GEZ-KiNO");
         
         setContentView(webView);
